801b4abef12d1a4bc9fc626519a4bd1a6d46912d
Dece Saneas <dece.saneas@gmail.com>
2021-08-31 20:31:57 +0700

    minor fix photo user

--- app/Http/Composers/DataComposer.php
+++ app/Http/Composers/DataComposer.php
@@ -3,8 +3,10 @@
 namespace App\Http\Composers;
 
 use Illuminate\View\View;
+use Auth;
 use Carbon\Carbon;
 use App\Models\Gelombang;
+use App\Models\Berkas;
 
 class DataComposer
 {
@@ -12,7 +14,8 @@ class DataComposer
     {
         $composer = [
             'gelombang' => Gelombang::orderBy('created_at', 'ASC')->get(),
-            'gelombang_now' => Gelombang::where('start_period', '<=', Carbon::now())->where('end_period', '>=', Carbon::now())->first()
+            'gelombang_now' => Gelombang::where('start_period', '<=', Carbon::now())->where('end_period', '>=', Carbon::now())->first(),
+            'berkas' => Berkas::where('user_id', Auth::id())->first()
         ];
         
         if(empty($composer['gelombang_now'])) {
